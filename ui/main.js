6eb309d003a1642329153c406a117279868c2997
Prabhakar98 <36556394+Prabhakar98@users.noreply.github.com>
2018-02-18 00:43:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,16 @@ button.onclick = function(){
   request.open('GET', 'http://u15eumt080.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document. getElementById('submit_button');
+submit.onclick = function(){
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var 1=0; i<names.length; i++){
+        list += '<li>' + names[i] +'<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
